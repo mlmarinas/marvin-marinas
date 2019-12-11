2e6c190118e6681f8b6794b85be9637dd5c5818b
Marvin <mlmarinas@gmail.com>
2019-12-10 22:20:43 -0700

    Added gulp file and sass folders

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+//Initialize Gulp Modules
+const gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps');
+
+
+const mainScssFile = './assets/stylesheets/main.scss',
+    cssDestinationFolder = './assets/stylesheets/';
+
+gulp.task('styles', function () {
+    return gulp.src(mainScssFile)
+        //Initialize sourcemaps
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        //gulp-autoprefixer
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        //Rename file 
+        .pipe(rename({ suffix: '.min' }))
+        //Write sourcemaps file to destination folder
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(cssDestinationFolder));
+        console.log(cssDestinationFolder);
+});
+
+//Watch
+const fileWatchPath = {
+    sass: './assets/stylesheets/**/*.scss'
+};
+
+function watchTask() {
+    return watch([fileWatchPath.sass],
+        gulp.parallel('styles'));
+}
+
+//Default task
+exports.default = gulp.series(
+    gulp.parallel('styles'),
+    watchTask);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
