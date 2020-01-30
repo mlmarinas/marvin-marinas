138e036e03383b6791054c2337c9c6763c816ee3
Marvin <mlmarinas@gmail.com>
2020-01-29 22:55:58 -0700

    Finished skills section of the index page

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,36 +1,33 @@
 <template>
-  <section class="work">
-    <h2 class="work__title"><strong>Here's what I can do.</strong></h2>
-    <p class="work__subtitle">
+  <section class="skills">
+    <h2 class="skills__title"><strong>Here's what I can do.</strong></h2>
+    <p class="skills__subtitle">
       If you have any special requests, feel free to contact me.
     </p>
-    <div class="work__grid work-grid">
-      <div class="work-grid__card work-card">
-        <i class="far fa-window-alt"></i>
-        <h3 class="work-card__title">Web Development</h3>
-        <p class="work-card__excerpt">
-          Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed
-          amet consequat lorem dolore.
+    <div class="skills__grid">
+      <div class="card">
+        <img src="../assets/images/browser.svg" class="skills__icon" />
+        <h3 class="card__title">Web Development</h3>
+        <p class="card__excerpt">
+          I can build you a responsive website, get you a domain name, and set up your own web server.
         </p>
       </div>
-      <div class="work-grid__card work-card">
-        <i class="fas fa-paint-brush-alt"></i>
-        <h3 class="work-card__title">Graphics</h3>
-        <p class="work-card__excerpt">
-          Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed
-          amet consequat lorem dolore.
+      <div class="card">
+        <img src="../assets/images/paint-brush.svg" class="skills__icon" />
+        <h3 class="card__title">Graphics</h3>
+        <p class="card__excerpt">
+          I can create and modify images and vectors with Adobe Illustrator, Adobe Photoshop, Affinity Photo, and Affinity Designer! 
         </p>
       </div>
-      <div class="work-grid__card work-card">
-        <i class="fas fa-crown"></i>
-        <h3 class="work-card__title">Branding</h3>
-        <p class="work-card__excerpt">
-          Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed
-          amet consequat lorem dolore.
+      <div class="card">
+        <img src="../assets/images/brand.svg" class="skills__icon" />
+        <h3 class="card__title">Branding</h3>
+        <p class="card__excerpt">
+          I can create a logo for your brand and set up your social media accounts for your web presence. 
         </p>
       </div>
     </div>
-    <div class="work__btn-box">
+    <div class="skills__btn-box">
       <a href="#" class="btn">
         View some of my work
       </a>
@@ -39,14 +36,19 @@
 </template>
 
 <style lang="scss" scoped>
-.work {
+.skills {
+  background: url("../assets/images/background.png");
+  background-size: repeat;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 14rem 0;
   box-shadow: 0 0.2rem 0.4rem 0 rgba(0, 0, 0, 0.15);
-
+  &__icon {
+    height: 50%;
+    margin-bottom: 2rem;
+  }
   &__title,
   &__subtitle {
     margin-bottom: 2.2rem;
@@ -57,12 +59,12 @@
   }
 
   &__btn {
-    width: 2.5rem;   
+    width: 2.5rem;
   }
 }
 
 /* Grid Container */
-.work-grid {
+.skills__grid {
   display: grid;
   margin-top: 3rem;
   grid-template-columns: repeat(3, 32rem);
@@ -71,32 +73,21 @@
 }
 
 /* Cards */
-.work-card {
+.card {
   background: white;
   box-shadow: 0 0.2rem 0.4rem 0 rgba(0, 0, 0, 0.15);
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 5rem 3.2rem 6rem 3.2rem;
+  padding: 3rem 2rem;
   border-radius: 0.4rem;
-
   &__title {
-    margin-bottom: 2rem;
+    margin-bottom: 1rem;
   }
-
   &__excerpt {
     text-align: center;
+    flex-grow: 2;
   }
-}
-
-/* Card Icons */
-.fa-window-alt,
-.fa-paint-brush-alt,
-.fa-crown {
-  font-weight: 400;
-  font-size: 10.3rem;
-  color: #1f74ac;
-  margin-bottom: 3rem;
 }
 </style>
